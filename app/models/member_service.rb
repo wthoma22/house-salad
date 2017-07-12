e04a193d345a110b98d21f018406b9c89415502e
William Thomas <wthoma390@gmail.com>
2017-07-12 10:30:26 -0600

    Update member_service to hit pry, hard code state until passing

--- app/models/member_service.rb
+++ app/models/member_service.rb
@@ -1,14 +1,14 @@
 class MemberService < ApplicationController
 
-  def initialize(house, state)
+  def initialize(state)
     @conn = Faraday.new(:url => "https://api.propublica.org/congress/v1/members")
-    @house = house
     @state = state
   end
 
-  def find_by_house
-    response = conn.get "/#{house}/#{state}/current.json" do |request|
+  def self.find_by_house
+    response = @conn.get "/house/CO/current.json" do |request|
       request.headers['Authorization'] = x-api-key
+      binding.pry
     end
     JSON.parse(response.body, symbolize_names: true)
   end
