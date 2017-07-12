2d8c73e0d43eac5c996246bb94a05f10ad9a11d5
William Thomas <wthoma390@gmail.com>
2017-07-12 10:17:38 -0600

    update correct names, create service model with faraday call, start working on sending key in header

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,13 +1,12 @@
 class Member
-  attr_reader :name, :role, :party, :districe
+  attr_reader :name, :role, :party, :district
   def initialize(attrs)
     @attrs = attrs
-    @name = attrs[:station_name]
+    @name = attrs[:name]
     @role = attrs[:role]
     @party = attrs[:party]
     @district = attrs[:district]
   end
-
 
   def self.find_by_house
     MemberService.find_by_house.map do |raw_member|
--- app/models/member_service.rb
+++ app/models/member_service.rb
@@ -1,4 +1,7 @@
-class MemberService
+class MemberService < ApplicationController
 
-  
+  def find_by_house
+    response = Faraday.get "https://api.propublica.org/congress/v1/members/house/CO/current.json"
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
