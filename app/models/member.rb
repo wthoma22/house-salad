2789d0c6cf5a7d19ea003d1355294620ccd7132d
William Thomas <wthoma390@gmail.com>
2017-07-12 10:11:38 -0600

    DELETE LAST COMMIT. Add attrs to member class, add member service model, key in yml file

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,18 +3,15 @@ class Member
   def initialize(attrs)
     @attrs = attrs
     @name = attrs[:station_name]
-    @fuel_type = attrs[:role]
-    @access_times = attrs[:party]
-    @distance = attrs[:district]
+    @role = attrs[:role]
+    @party = attrs[:party]
+    @district = attrs[:district]
   end
 
-  def address
-    "#{attrs[:street_address]} #{attrs[:city]}, #{attrs[:state]} #{attrs[:zip]}"
-  end
 
-  def self.find_nearest(filter)
-    NrelService.find_nearest(filter).map do |raw_station|
-      new(raw_station)
+  def self.find_by_house
+    MemberService.find_by_house.map do |raw_member|
+      new(raw_member)
     end
   end
 
