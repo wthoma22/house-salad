e611320728d1612969fbd42b6ca3d4d4f968bd3c
William Thomas <wthoma390@gmail.com>
2017-07-12 11:10:10 -0600

    add filter for limit

--- app/models/member_service.rb
+++ app/models/member_service.rb
@@ -1,7 +1,7 @@
 class MemberService < ApplicationController
-  attr_reader :conn
 
-  def initialize(state)
+  def initialize(filter = {})
+    @filter = filter
     @conn = Faraday.new(url: "https://api.propublica.org/congress/v1/members") do |faraday|
       faraday.headers["X-API-KEY: #{ENV['x-api-key']}"]
     @state = state
@@ -15,5 +15,11 @@ class MemberService < ApplicationController
   end
 
 private
-  # attr_reader :conn
+  attr_reader :conn, :filter
+
+  def filter
+    {
+      limit: 7
+    }
+  end
 end
