f947add9c672c8cf73553f958b46251111f992e8
William Thomas <wthoma390@gmail.com>
2017-07-12 10:23:48 -0600

    Add initialize and find_by_house method to member service

--- app/models/member_service.rb
+++ app/models/member_service.rb
@@ -1,7 +1,15 @@
 class MemberService < ApplicationController
 
+  def initialize(house, state)
+    @conn = Faraday.new(:url => "https://api.propublica.org/congress/v1/members")
+    @house = house
+    @state = state
+  end
+
   def find_by_house
-    response = Faraday.get "https://api.propublica.org/congress/v1/members/house/CO/current.json"
+    response = conn.get "/#{house}/#{state}/current.json" do |request|
+      request.headers['Authorization'] = x-api-key
+    end
     JSON.parse(response.body, symbolize_names: true)
   end
 end
