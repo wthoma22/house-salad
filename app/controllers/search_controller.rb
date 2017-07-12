b0c301835dc43214e2188deb5d36f0b59fa732d6
William Thomas <wthoma390@gmail.com>
2017-07-12 10:02:14 -0600

    API key moved to yml file

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,3 @@ class SearchController < ApplicationController
     @members = Member.find_by_house(state: params[:search])
   end
 end
-
-# Postman
-# https://api.propublica.org/congress/v1/members/house/CO/current.json
-# x-api-key fs7GLfE4863GiTsnS3qkw94uScKbrlmZ8GrNYbrJ
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,23 @@
 class Member
-  
+  attr_reader :name, :role, :party, :districe
+  def initialize(attrs)
+    @attrs = attrs
+    @name = attrs[:station_name]
+    @fuel_type = attrs[:role]
+    @access_times = attrs[:party]
+    @distance = attrs[:district]
+  end
+
+  def address
+    "#{attrs[:street_address]} #{attrs[:city]}, #{attrs[:state]} #{attrs[:zip]}"
+  end
+
+  def self.find_nearest(filter)
+    NrelService.find_nearest(filter).map do |raw_station|
+      new(raw_station)
+    end
+  end
+
+private
+  attr_reader :attrs
 end
