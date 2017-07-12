11c43bfe03bd941d5fe0f618abe9b15be284d9d9
William Thomas <wthoma390@gmail.com>
2017-07-12 10:46:37 -0600

    Add conn instance variable to member_service and move request block up to initialize

--- app/models/member_service.rb
+++ app/models/member_service.rb
@@ -1,14 +1,14 @@
 class MemberService < ApplicationController
 
   def initialize(state)
-    @conn = Faraday.new(:url => "https://api.propublica.org/congress/v1/members")
+    @conn = Faraday.new(url: "https://api.propublica.org/congress/v1/members") do |faraday|
+      faraday.headers["X-API-KEY: #{ENV["x-api-key"]}"]
     @state = state
   end
 
   def self.find_by_house
-    response = @conn.get "/house/CO/current.json" do |request|
-      request.headers['Authorization'] = x-api-key
-      binding.pry
+    response = @conn.get "/house/CO/current.json"
+    binding.pry
     end
     JSON.parse(response.body, symbolize_names: true)
   end
