3916b2f191021071e57ad6092648548bdff6bce4
William Thomas <wthoma390@gmail.com>
2017-07-12 09:59:26 -0600

    Recieved correct response with Postman and added method in controller to place in member model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,10 @@
 class SearchController < ApplicationController
 
   def index
-
+    @members = Member.find_by_house(state: params[:search])
   end
 end
+
+# Postman
+# https://api.propublica.org/congress/v1/members/house/CO/current.json
+# x-api-key fs7GLfE4863GiTsnS3qkw94uScKbrlmZ8GrNYbrJ
