768b05f70a0bc806f6cb5c1ce9547a528f3b83b1
William Thomas <wthoma390@gmail.com>
2017-07-12 09:35:02 -0600

    updte gemfile and add first spec draft for user house member searches

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem 'capybara'
+  gem 'pry-rails'
 end
 
 group :development do
---
+++ spec/user_searches_for_house_members_by_state_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+feature "User can view house members by state" do
+  scenario "it is a valid search" do
+    # As a user
+    # When I visit "/"
+    visit "/"
+    # And I select "Colorado" from the dropdown
+    select("Colorado", :from => "us_states").select_option
+    # And I click on "Locate Members from the House"
+    click_on "Locate Members from the House"
+    # Then my path should be "/search" with "state=CO" in the parameters
+    expect(current_path).to eq("/search?state=CO")
+    # And I should see a message "7 Results"
+    expect(page).to have_selector(".members", count: 7)
+    # And I should see a list of 7 the members of the house for Colorado
+    # And they should be ordered by seniority from most to least
+    # And I should see a name, role, party, and district for each member```
+    within first(".member") do
+      expect(page).to have_selector(".member_name")
+      expect(page).to have_selector(".member_role")
+      expect(page).to have_selector(".member_party")
+      expect(page).to have_selector(".member_district")
+    end
+  end
+end
